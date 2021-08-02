9bf03029f286fdbab08888463d92eb5cce9ee4ea
RenanParohinog4 <renanparohinog@gmail.com>
2021-08-03 00:27:09 +0800

    first commit

---
+++ project-tracking-intro-component-master/main.js
@@ -0,0 +1,9 @@
+const menuIcon = document.querySelector('.hamburger-menu');
+
+const navBar = document.querySelector('.nav-bar ')
+
+menuIcon.addEventListener('click', () =>{
+    navBar.classList.toggle('change')
+})
+
+//Guys im just practicing thats why my code is not clean and time will come I will improve lettsssss goooo!
